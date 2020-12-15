e453608510b3d23ecabd96506fe365d14b7d513b
Bernardo-Quina <bernardoquina@gmail.com>
2020-12-15 15:48:11 +0000

    Class inheritance & protected modifier & getters & setters

--- src/ClassBasics.ts
+++ src/ClassBasics.ts
@@ -2,7 +2,7 @@ class Department {
   // private readonly id: string
   // private name: string
 
-  private employees: string[] = [] // private: Typescript feature only in compilation
+  protected employees: string[] = [] // private: Typescript feature only in compilation
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id
@@ -24,18 +24,69 @@ class Department {
   }
 }
 
+// Inheriting and extending classes
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, 'IT')
+  }
+}
 
-const accounting = new Department('d1','Accounting')
+class accountingDepartment extends Department {
+  private lastReport: string
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport
+    }
+    throw new Error('No report found.')
+  }
+
+  set mostRecentReport(text: string) {
+    if (!text) {
+      throw new Error('Please pass in a valid text!')
+    }
+    this.addReport(text)
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, 'ACC')
+    this.lastReport = reports[0]
+  }
+
+  addEmployee(name: string) {
+    if (name === 'Max') {
+      return
+    }
+    this.employees.push(name)
+  }
+
+  addReport(text: string) {
+    this.reports.push(text)
+    this.lastReport = text
+  }
+
+  printReports() {
+    console.log(this.reports)
+  }
+}
+
+const accounting = new accountingDepartment('d1',['Initial accounting report'])
 
 console.log(accounting.name)
-
+accounting.mostRecentReport = 'new report coming soon'
+accounting.printReports()
 accounting.addEmployee('Bernardo')
 accounting.addEmployee('Rita')
+accounting.addEmployee('Max')
+console.log(accounting.mostRecentReport)
 
 // accounting.employees[2] = 'Mariana' this is possible if the field isn't private 
 
 accounting.describe()
 accounting.printEmployeeInfo()
+
+const it = new ITDepartment('d2', ['Bernardo', 'Rita'])
+console.log(it)
 
 // const accountingCopy = { describe: accountingDepartment.describe, name: 's' }
 
