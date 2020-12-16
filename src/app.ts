9af2009c92256d74955017060ab4e407dde1b060
Bernardo-Quina <bernardoquina@gmail.com>
2020-12-16 21:46:44 +0000

    Type casting & index properties & function overloads & optional chaining & nullish coalescing

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,11 @@ type Numeric = number | boolean
 
 type Universal = Combinable & Numeric // Type number because that is the intersection
 
+// Function overloads
+function add(a: number, b: number): number
+function add(a: string, b:number): string
+function add(a: number, b:string): string
+function add(a: string, b: string): string
 function add(a: Combinable, b: Combinable) {
   // This is a type guard
   if (typeof a === 'string' || typeof b === 'string') {
@@ -30,6 +35,21 @@ function add(a: Combinable, b: Combinable) {
   
   return a + b
 }
+
+const result = add('bernardo', 5) // typescript is able to infer result as a string
+
+const fetchedUserData = {
+  id: 'u1',
+  name: 'Bernardo',
+  job: { title: 'CEO', description: 'My own company' }
+}
+
+console.log(fetchedUserData?.job?.title) // Typescript optional chaining
+
+const userInput2 = ''
+
+const storedData = userInput2 ?? 'DEFAULT' // Nullish coalescing operator
+
 
 type UnknownEmployee = Employee | Admin
 
@@ -109,3 +129,21 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({type: 'bird', flyingSpeed: 10})
+
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!
+
+const userInputElement = document.getElementById('user-input')
+
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = 'Hi there!'
+}
+
+interface ErrorContainer { // { email: 'Not a valid email', username: 'Already taken' }
+  [prop: string]: string
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email',
+  username: 'Already taken'
+}
+
