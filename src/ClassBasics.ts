c9ddaa1b3771c92bbc48feb4c88a94fc7316cb90
Bernardo-Quina <bernardoquina@gmail.com>
2020-12-16 11:12:19 +0000

    Static methods and properties & abstract classes & singletons private constructors

--- src/ClassBasics.ts
+++ src/ClassBasics.ts
@@ -1,17 +1,24 @@
-class Department {
+abstract class Department {
+  
   // private readonly id: string
   // private name: string
 
   protected employees: string[] = [] // private: Typescript feature only in compilation
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
     // this.id = id
     // this.name = n
   }
 
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name}`)
+  // Static methods and properties
+  static fiscalYear = 2020
+
+  static createEmployee(name: string) {
+    return { name }
   }
+
+  // forces extending classes to implement methods/properties
+  abstract describe(this: Department): void
 
   addEmployee(employee: string) {
     // this.id = 'd2' Cannot assign to 'id' because it is a read-only property
@@ -29,10 +36,19 @@ class ITDepartment extends Department {
   constructor(id: string, public admins: string[]) {
     super(id, 'IT')
   }
+
+  describe() {
+    console.log('IT Department - ID: ' + this.id)
+  }
 }
 
-class accountingDepartment extends Department {
+class AccountingDepartment extends Department {
   private lastReport: string
+  private static instance: AccountingDepartment
+
+  describe() {
+    console.log('Accounting Department - ID: ' + this.id)
+  }
 
   get mostRecentReport() {
     if (this.lastReport) {
@@ -48,9 +64,17 @@ class accountingDepartment extends Department {
     this.addReport(text)
   }
 
-  constructor(id: string, private reports: string[]) {
+  private constructor(id: string, private reports: string[]) {
     super(id, 'ACC')
     this.lastReport = reports[0]
+  }
+
+  static getInstance() {
+    if (AccountingDepartment.instance) {
+      return this.instance
+    }
+    this.instance = new AccountingDepartment('d2', ['Initial report.'])
+    return this.instance
   }
 
   addEmployee(name: string) {
@@ -70,23 +94,33 @@ class accountingDepartment extends Department {
   }
 }
 
-const accounting = new accountingDepartment('d1',['Initial accounting report'])
+const accounting = AccountingDepartment.getInstance()
+const accounting2 = AccountingDepartment.getInstance()
 
-console.log(accounting.name)
-accounting.mostRecentReport = 'new report coming soon'
-accounting.printReports()
-accounting.addEmployee('Bernardo')
-accounting.addEmployee('Rita')
-accounting.addEmployee('Max')
+console.log(accounting, accounting2)
+
+// console.log(accounting.name)
+// accounting.mostRecentReport = 'new report coming soon'
+// accounting.printReports()
+// accounting.addEmployee('Bernardo')
+// accounting.addEmployee('Rita')
+// accounting.addEmployee('Max')
 console.log(accounting.mostRecentReport)
 
 // accounting.employees[2] = 'Mariana' this is possible if the field isn't private 
 
 accounting.describe()
-accounting.printEmployeeInfo()
+// accounting.printEmployeeInfo()
+
+// using static method
+const employee1 = Department.createEmployee('Max')
+console.log(employee1)
 
 const it = new ITDepartment('d2', ['Bernardo', 'Rita'])
+
+
 console.log(it)
+it.describe()
 
 // const accountingCopy = { describe: accountingDepartment.describe, name: 's' }
 
