acaf0d5042f7812e6049ad9d33f67f6e05cb32d3
Bernardo-Quina <bernardoquina@gmail.com>
2020-12-17 16:18:51 +0000

    Generic classes, generic utility types & generic types vs union types

--- src/app.ts
+++ src/app.ts
@@ -49,3 +49,64 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 console.log(extractAndConvert({ name: 'Bernardo' }, 'name'))
+
+// Generic Classes
+class DataStorage<T extends string | number | boolean> {
+  // data: (string | number | boolean)[] would not work the same because this allows
+  // mixed types when creating an instance as well as when using methods
+  private data: T[] = []
+
+  addItem(item: T) {
+    this.data.push(item)
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+
+  getItems() {
+    return [...this.data]
+  }
+}
+
+const textStorage = new DataStorage<string>()
+textStorage.addItem('Bernardo')
+textStorage.addItem('Rita')
+textStorage.removeItem('Bernardo')
+console.log(textStorage.getItems())
+
+const numberStorage = new DataStorage<number>()
+numberStorage.addItem(1)
+numberStorage.addItem(5)
+numberStorage.addItem(100)
+numberStorage.removeItem(5)
+console.log(numberStorage.getItems())
+
+// const objectStorage = new DataStorage<object>() not allowed since removeItem fails
+// objectStorage.addItem({name: 'Bernardo'})
+// objectStorage.addItem({name: 'Rita'})
+// objectStorage.removeItem({name: 'Bernardo'}) doesn't work
+// console.log(objectStorage.getItems())
+
+// Generic utility types
+// Partial
+interface CourseGoal {
+  title: string
+  description: string
+  completeUntil: Date
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {}
+  courseGoal.title = title
+  courseGoal.description = description
+  courseGoal.completeUntil = date
+
+  return courseGoal as CourseGoal
+}
+
+// Readonly
+const namesToRead: Readonly<string[]> = ['Bernardo', 'Rita']
+
+// namesToRead.push('Francisco') Property 'push' does not exist on type 'readonly...
+
