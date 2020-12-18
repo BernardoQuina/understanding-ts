35f8596ac3edf6547eb2665839a71779ac18ccb2
Bernardo-Quina <bernardoquina@gmail.com>
2020-12-18 18:35:20 +0000

    Adding multiple decorators & property, accessor, method & parameter decorators

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 // Decorators
+
+
 function Logger(logString: string) {
   return function(constructor: Function) {
     console.log(logString)
@@ -7,6 +9,7 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(selector: string, hookId: string) {
+  console.log('Rendering template')
   return function (constructor: any) {
     const hookEl = document.getElementById(hookId)
     const p = new constructor()
@@ -18,6 +21,7 @@ function WithTemplate(selector: string, hookId: string) {
 }
 
 @WithTemplate('h1', 'app')
+@Logger('Logging') // you can use multiple decorators: executed bottom up
 class PersonDecorators {
   name = 'Bernardo'
 
@@ -29,3 +33,60 @@ class PersonDecorators {
 const pers = new PersonDecorators
 
 console.log(pers)
+
+// -----
+
+// property decorator
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property decorator')
+  console.log(target, propertyName)
+}
+
+// accessor decorator
+function Log2(target: any, name: string | Symbol, descriptor: PropertyDescriptor){
+  console.log('Accessor decorator')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+// method decorator (similar to accessor)
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
+  console.log('Method decorator')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+// parameter decorator
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('Parameter decorator')
+  console.log(target)
+  console.log(name)
+  console.log(position)
+}
+
+class Product {
+  @Log
+  title: string
+  private _price: number
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val
+    } else {
+      throw new Error('Invalid price: should be positive.')
+    }
+  }
+
+  constructor(title: string, price: number) {
+    this.title = title
+    this._price = price
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax)
+  }
+}
