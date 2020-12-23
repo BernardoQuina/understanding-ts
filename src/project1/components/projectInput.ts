9767debae924c4bc38baca363a9be440b678ed1b
Bernardo-Quina <bernardoquina@gmail.com>
2020-12-23 13:18:58 +0000

    Working with namespaces, setting outFile in tsconfig & organizing files & folders

---
+++ src/project1/components/projectInput.ts
@@ -0,0 +1,92 @@
+/// <reference path="baseComponent.ts" />
+/// <reference path="../util/validation.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../state/projectState.ts" />
+
+
+// ProjectInput class
+
+namespace App {
+  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
+
+    constructor() {
+      super('project-input', 'app', true, 'user-input')
+
+      this.configure()
+    }
+
+    configure() {
+      this.titleInputElement = this.element.querySelector(
+        '#title'
+      ) as HTMLInputElement
+      this.descriptionInputElement = this.element.querySelector(
+        '#description'
+      ) as HTMLInputElement
+      this.peopleInputElement = this.element.querySelector(
+        '#people'
+      ) as HTMLInputElement
+
+      this.element.addEventListener('submit', this.submitHandler)
+    }
+
+    renderContent() {}
+
+    private gatherUserInput(): [string, string, number] | void {
+      const enteredTitle = this.titleInputElement.value
+      const enteredDescription = this.descriptionInputElement.value
+      const enteredPeople = this.peopleInputElement.value
+
+      const titleValidatable: Validatable = {
+        value: enteredTitle,
+        required: true,
+      }
+
+      const descriptionValidatable: Validatable = {
+        value: enteredDescription,
+        required: true,
+        minLength: 5,
+        maxLength: 100,
+      }
+
+      const peopleValidatable: Validatable = {
+        value: +enteredPeople,
+        required: true,
+        min: 1,
+        max: 10,
+      }
+
+      if (
+        !Validate(titleValidatable) ||
+        !Validate(descriptionValidatable) ||
+        !Validate(peopleValidatable)
+      ) {
+        alert('Invalid input, please try again!')
+        return
+      } else {
+        return [enteredTitle, enteredDescription, +enteredPeople]
+      }
+    }
+
+    private clearInputs() {
+      this.titleInputElement.value = ''
+      this.descriptionInputElement.value = ''
+      this.peopleInputElement.value = ''
+    }
+
+    @Autobind
+    private submitHandler(event: Event) {
+      event.preventDefault()
+      console.log(this.titleInputElement.value)
+      const userInput = this.gatherUserInput()
+      if (Array.isArray(userInput)) {
+        const [title, description, people] = userInput
+        projectState.addProject(title, description, people)
+        console.log(title, description, people)
+      }
+      this.clearInputs()
+    }
+  }
+}
---
+++ src/project1/components/projectList.ts
@@ -0,0 +1,92 @@
+/// <reference path="baseComponent.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../state/projectState.ts" />
+/// <reference path="../models/project.ts" />
+/// <reference path="../models/dragDropInterfaces.ts" />
+
+// ProjectList Class
+
+namespace App {
+  export class ProjectList
+    extends Component<HTMLDivElement, HTMLElement>
+    implements DragTarget {
+    assignedProjects: Project[]
+
+    constructor(private type: 'active' | 'finished') {
+      super('project-list', 'app', false, `${type}-projects`)
+
+      this.assignedProjects = []
+
+      this.configure()
+
+      this.renderContent()
+    }
+
+    @Autobind
+    dragOverHandler(event: DragEvent) {
+      if (event.dataTransfer && event.dataTransfer.types[0] === 'text/plain') {
+        event.preventDefault()
+        this.element.classList.add('droppable')
+        const elementUl = this.element.querySelector('ul')!
+        elementUl.classList.add('droppableBackground')
+      }
+    }
+
+    @Autobind
+    dragLeaveHandler(_event: DragEvent) {
+      this.element.classList.remove('droppable')
+      const elementUl = this.element.querySelector('ul')!
+      elementUl.classList.remove('droppableBackground')
+    }
+
+    @Autobind
+    dropHandler(event: DragEvent) {
+      event.preventDefault()
+      const prjId = event.dataTransfer!.getData('text/plain')
+
+      projectState.moveProject(
+        prjId,
+        this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished
+      )
+
+      const elementUl = this.element.querySelector('ul')!
+      elementUl.classList.remove('droppableBackground')
+    }
+
+    configure() {
+      this.element.addEventListener('dragover', this.dragOverHandler)
+      this.element.addEventListener('dragleave', this.dragLeaveHandler)
+      this.element.addEventListener('drop', this.dropHandler)
+      projectState.addListener((projects: Project[]) => {
+        const filteredProjects = projects.filter((prj) => {
+          if (this.type === 'active') {
+            return prj.status === ProjectStatus.Active
+          } else {
+            return prj.status === ProjectStatus.Finished
+          }
+        })
+        this.assignedProjects = filteredProjects
+        this.renderProjects()
+      })
+    }
+
+    renderContent() {
+      const listId = `${this.type}-projects-list`
+      this.element.querySelector('ul')!.id = listId
+      this.element.querySelector('h2')!.textContent =
+        this.type.toUpperCase() + ' PROJECTS'
+    }
+
+    private renderProjects() {
+      const listEl = document.getElementById(
+        `${this.type}-projects-list`
+      )! as HTMLUListElement
+
+      listEl.innerHTML = ''
+
+      for (const prjItem of this.assignedProjects) {
+        new ProjectItem(this.element.querySelector('ul')!.id, prjItem)
+      }
+    }
+  }
+}
