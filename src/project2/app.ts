b5c7d1bb36fd8f8e27af4f35947889899958d0e9
Bernardo-Quina <bernardoquina@gmail.com>
2020-12-26 16:18:27 +0000

    Rendering a map with google maps & including google maps types

--- src/project2/app.ts
+++ src/project2/app.ts
@@ -26,6 +26,14 @@ async function searchAddressHandler(event: Event) {
     }
 
     const coordinates = response.data.results[0].geometry.location
+
+    const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      center: coordinates,
+      zoom: 8,
+    })
+
+    new google.maps.Marker({position: coordinates, map: map})
+
   } catch (err) {
     alert(err.message)
     console.log(err)
