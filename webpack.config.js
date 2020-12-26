300cf018226bd840a5747a28783095e0a0ebeceb
Bernardo-Quina <bernardoquina@gmail.com>
2020-12-26 12:12:00 +0000

    Project 2 setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: './src/thirdPartyLibs/app.ts',
+  entry: './src/project2/app.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
